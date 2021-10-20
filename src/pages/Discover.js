44700d65b87fa12120696e9527aa2c26e47ba6fc
Nazar Dzhura <milkysdave@gmail.com>
2021-10-20 22:14:08 +0300

    add: implement filtering films by genres

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,26 +1,43 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {API_KEY} from "../utils/consts";
 import {useAxiosGet} from "../hooks/httpRequests";
 import Loader from "../components/Loader";
 import FilmCard from "../components/film-card/FilmCard";
 import Container from "@mui/material/Container";
 import PaginationRounded from "../components/Pagination";
-import DropSelector from "../components/DropSelector";
+import DropSelector from "../components/selectors/DropSelector";
+import MultipleSelector from "../components/selectors/MultipleSelector";
 
 const Discover = () => {
-    const [releaseDateSortValue, setReleaseDateSortValue] = useState('asc');
     const [page, setPage] = useState(1)
+    const [releaseDateSortValue, setReleaseDateSortValue] = useState('asc');
+    const [genreSelectorValues, setGenreSelectorValues] = useState([])
+    const [genreUrlSlice, setGenreUrlSlice] = useState('')
     let content = null
 
     let discoverUrl = 'https://api.themoviedb.org/3/discover/movie' +
         '?api_key=' + API_KEY +
         '&page=' + page +
-        '&sort_by=release_date.' + releaseDateSortValue
+        '&sort_by=release_date.' + releaseDateSortValue +
+        '&with_genres=' + genreUrlSlice
     ;
     let genresUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`
 
     let films = useAxiosGet(discoverUrl)
     let genres = useAxiosGet(genresUrl)
+
+    useEffect(() => {
+        let wantedGenresIds = ''
+        for (const genreSelectorValue of genreSelectorValues) {
+            for (const genre of genres.data.genres) {
+                if (genre.name === genreSelectorValue) {
+                    wantedGenresIds += genre.id + '%2C'
+                }
+            }
+        }
+        setGenreUrlSlice(wantedGenresIds)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [genreSelectorValues]);
 
     if (films.error) {
         content = <div className="bg-blue-300 mb-2 p-3 rounded">
@@ -43,6 +60,7 @@ const Discover = () => {
     return (
         <div>
             <Container sx={{paddingTop: '50px'}} maxWidth="lg">
+                {/*<Header />*/}
                 <Container sx={{paddingBottom: '50px'}} maxWidth="lg">
                     <DropSelector
                         select={releaseDateSortValue}
@@ -51,7 +69,14 @@ const Discover = () => {
                         values={["asc", "desc"]}
                         label="Sort by release date"
                     />
-                        { content }
+                    <MultipleSelector
+                        select={genreSelectorValues}
+                        setSelect={setGenreSelectorValues}
+                        label="Filter by genres"
+                        placeholder="Select"
+                        options={genres.data ? genres.data.genres.map(item => item.name) : []}
+                        values={genres.data ? genres.data.genres.map(item => item.name) : []}/>
+                    { content }
                     <PaginationRounded setPage={setPage} countOfPages={films.data ? films.data.total_pages : 1}/>
                 </Container>
             </Container>
